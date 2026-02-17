44986f2af2f27b31e5388df63ce22b39353fc495
rammolankula <ramu.molankula.poorna@gmail.com>
2026-02-17 12:00:12 +0530

    updated files

--- BackEnd/src/test/java/manu/interview/ManuTechApplicationTests.java
+++ BackEnd/src/test/java/manu/interview/ManuTechApplicationTests.java
@@ -1,13 +1,13 @@
-package manu.interview;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class ManuTechApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package manu.interview;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class ManuTechApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
--- frontend/src/app.js
+++ frontend/src/app.js
@@ -4,7 +4,18 @@ import{useState,useEffect}from'react';
 
 function app(){
 
+
 const[members,setMembers]=useState([]);
+const[selectedMembers,setSelectedMembers]=useState(null);
+const[workOuts,workOuts]=useState([]);
+const[formData,setFormDate]=useState({
+exerciseName:",
+sets:",
+reps:",
+weigts:"
+})
+
+useStat
 
 
 }
